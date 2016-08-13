ce550d44a3a27214e7bf52b2b7c7cc1f20b726c5
ogom <ogom@outlook.com>
2016-08-14 00:23:26 +0900

    Add output_path configuration to graph generator

--- test/graph_test.rb
+++ test/graph_test.rb
@@ -5,11 +5,23 @@ require 'test_helper'
 class GraphTest < ActiveSupport::TestCase
   def test_graph
     FileUtils.rm_f Rails.root.join('Bug.png')
+    FileUtils.rm_f Rails.root.join('.smdconfig')
 
     Dir.chdir Rails.root do
       `rails g stateful_enum:graph bug`
     end
 
     assert File.exist?(Rails.root.join('Bug.png'))
+  end
+
+  def test_output_path
+    FileUtils.rm_f Rails.root.join('doc', 'Bug.png')
+    File.write(Rails.root.join('.smdconfig'), 'output_path: doc')
+
+    Dir.chdir Rails.root do
+      `rails g stateful_enum:graph bug`
+    end
+
+    assert File.exist?(Rails.root.join('doc', 'Bug.png'))
   end
 end
